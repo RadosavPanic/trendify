92c4dd1e60f5d32ac521057ff365f72df63a12dc
Radosav Panic <radosav.panic01@gmail.com>
2025-03-31 10:20:40 +0200

    setting up sanity schemas and clients

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -3,8 +3,7 @@ import { Button } from "@/components/ui/button";
 const Home = () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Hello world</h1>
-      <Button variant="outline">Button</Button>
+      <Button variant="outline">Click me</Button>
     </div>
   );
 };
--- sanity/lib/backendClient.ts
+++ sanity/lib/backendClient.ts
@@ -6,9 +6,5 @@ export const client = createClient({
   dataset,
   apiVersion,
   useCdn: true,
-  stega: {
-    studioUrl: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/studio`
-      : `${process.env.NEXT_PUBLIC_BASE_URL}/studio`,
-  },
+  token: process.env.SANITY_API_TOKEN,
 });
