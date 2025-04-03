bbf366a76ccecbcd7502cc0ac4225ed4053913e9
Radosav Panic <radosav.panic01@gmail.com>
2025-04-03 21:04:10 +0200

    integrating stripe payments with server actions

--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -24,7 +24,7 @@ const AddToCartButton = ({ product, disabled }: AddToCartButtonTypes) => {
   return (
     <div className="flex items-center justify-center space-x-2">
       <button
-        className={`w-8 h-8 rounded-full flex items-center justify-center transition-colors duration-200 ${itemCount === 0 ? "bg-gray-100 cursor-not-allowed" : "bg-gray-200 hover:bg-gray-300"}`}
+        className={`w-8 h-8 rounded-full flex items-center justify-center transition-colors duration-200 ${itemCount === 0 ? "bg-gray-100 cursor-not-allowed" : "bg-gray-200 hover:bg-gray-300 cursor-pointer"}`}
         disabled={itemCount === 0 || disabled}
         onClick={() => removeItem(product._id)}
       >
@@ -36,8 +36,7 @@ const AddToCartButton = ({ product, disabled }: AddToCartButtonTypes) => {
       </button>
       <span className="w-8 text-center font-semibold">{itemCount}</span>
       <button
-        className={`w-8 h-8 rounded-full flex items-center justify-center transition-colors duration-200 ${disabled ? "bg-gray-400 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600"}`}
-        disabled={itemCount === 0 || disabled}
+        className={`w-8 h-8 rounded-full flex items-center justify-center transition-colors duration-200 ${disabled ? "bg-gray-400 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600 cursor-pointer"}`}
         onClick={() => addItem(product)}
       >
         <span className="text-xl font-bold text-white">+</span>
